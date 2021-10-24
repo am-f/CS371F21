f2226a94a9c94d8df09871f5ca6d54974a1c5058
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-10-24 16:01:14 -0400

    comments

--- project1/task3/Block.java
+++ project1/task3/Block.java
@@ -3,6 +3,7 @@ public class Block implements Comparable<Block> {
     //Note from Allison: I'm not sure if putting private in front of these attributes is the right/standard way to do this, but if we
     // keep the class public and don't modify variable access at all then the attributes will be public, which we don't want (I don't
     // think).  We also need to do something similar to any class currently declared public if we want private attributes in it
+    //JYM: yes. unless you define Block as protected/package level access, by default, always private attributes
     private int offset;
     private int size;
     private Block left;
