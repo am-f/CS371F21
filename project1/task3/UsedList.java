0c4ae78b62eb2eb35db3309abac4723a8d1308f4
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-10-24 16:07:58 -0400

    Update UsedList.java

--- project1/task3/UsedList.java
+++ project1/task3/UsedList.java
@@ -8,6 +8,9 @@ public class UsedList extends BlockList {
         //does used need memSize? Or just free?
         //also do we actually need maxSize and totalSize for usedlist? would it be the largest block of used memory
         // and amount of total memory used??
+        //JYM: Imaging a scenario you just initialize your MemoryAllocation. You have a big contiguous space of 1 to 100 bytes. 
+        // If you are using Block [1,100] to represent it, then it is essentially constructing a node, and inserting it into
+        // list. That is it. For list it is just a block node. Why does list have to know totalSize or maxSize?
     }
     
 }
